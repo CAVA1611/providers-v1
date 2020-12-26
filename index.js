5e2a95a26ea6d6b8a252cd1ebf2497049625f00a
Cesar Velasco Arias <cesar091611pm@gmail.com>
2020-12-26 02:42:33 +0000

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var DataStore = require("nedb");
 
 var port = 4500;
 var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname+"/provider-v1.json";
 
 var providers = [
     {"cif": "A58818501", "name": "Proveedor 1", "address": "Sevilla", "cp":"20011", "phone": "0976543234", "email":"provedor1@gmail.com"},
@@ -14,6 +16,12 @@ console.log("Starting API Server...");
 
 var app = express();
 app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true 
+});
+
 
 app.get("/", (req, res) => {
     res.send("<html><body><h1>Microservicio Providers !!</h1></body></html>");
@@ -27,6 +35,15 @@ app.get(BASE_API_PATH + "/providers", (req, res) =>{
 app.post(BASE_API_PATH + "/providers", (req, res) => {
     console.log(Date() + "- POST /providers");
     var provider = req.body;
+    db.insert(providers,(err)=>{
+        if (err) {
+            console.log(Date() + "-" + err );
+            res.sendStatus(500);
+        }else {
+            res.sendStatus(201);
+        }
+
+    });
     providers.push(provider);
     res.sendStatus(201);
 });
