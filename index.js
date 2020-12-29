bb31724a8ce535f72afd78edec6d6f3d5bd90b39
Cesar Velasco Arias <cesar091611pm@gmail.com>
2020-12-29 12:50:07 +0000

    Added support for DELETE method

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.post(BASE_API_PATH + "/providers", (req, res) => {
     });
 });
 
+//actualizar email en base al id (cif) de cada proveedor
 app.put(BASE_API_PATH + "/providers" + "/:id" + "/email", (req, res) => {
     console.log(Date() + "- DELETE /providers/id/email");
     var email_prov = req.body.email;
@@ -65,6 +66,35 @@ app.put(BASE_API_PATH + "/providers" + "/:id" + "/email", (req, res) => {
     });
 });
 
+//DELETE Method - Elimina todos los registros que se han ingresado con el POST
+
+app.delete(BASE_API_PATH + "/providers", (req, res) => {
+    console.log(Date() + "- DELETE /providers");
+    db.remove({}, {multi: true}, (err) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+    
+//DELETE Method - Elimina 1 proveedor
+
+app.delete(BASE_API_PATH + "/providers" + '/:id', (req, res) => {
+    console.log(Date() + "- DELETE /providers/id");
+    var cifID = req.params.id;
+    db.remove({cif: cifID}, (err) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res,sendStatus(500);
+        } else {
+            res.sendStatus(200);
+            console.log("Se elimino el Provedor con CIF: " + cifID);
+        }
+    });
+});
 
 
 app.listen(port);
