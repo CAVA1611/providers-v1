ff5f3231668657ae6fb1144ba554e6e88b32e4e4
Cesar Velasco <cesar091611pm@gmail.com>
2021-01-26 16:03:45 +0000

    update schema db

--- providers.js
+++ providers.js
@@ -1,14 +1,41 @@
 const mongoose = require('mongoose');
 
 const providerSchema = new mongoose.Schema({
-    cif: {type: String, minlength: 9}, 
-    name: String, 
-    address: String,
-    cp: Number,
-    phone: Number,
-    email: String,
-    code: String,
-    stock_sale: Number
+    cif: {
+        type: String, 
+        minlength: 9,
+        required: true
+        }, 
+    name: {
+        type: String,
+        required: true
+        }, 
+    address: {
+        type: String,
+        required: true
+        },
+    cp: {
+        type: Number,
+        minlength: 5,
+        required: true
+        },
+    phone: {
+        type: Number,
+        required: true
+        },
+    email: {
+        type: String,
+        required: true,
+        match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+      },
+    code: {
+        type: String,
+        required: true
+        },
+    stock_sale: {
+        type: Number,
+        required: true
+    }
 });
 
 providerSchema.methods.cleanup = function() {
