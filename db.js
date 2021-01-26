7f97342e5f931b9187d9588d329948a03fac2c40
Cesar Velasco <cesar091611pm@gmail.com>
2021-01-26 14:15:57 +0000

    Added comand for test on travis

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const DB_URL = (process.env.MONGO_URL || 'mongodb://db/test');
 
+
 const dbConnect = function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
